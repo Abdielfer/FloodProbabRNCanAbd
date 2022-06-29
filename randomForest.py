55b30eaef685a97d977aecc79e411eed3d7cebd0
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2022-06-29 15:23:06 -0400

    random forest firsts methods

--- randomForest.py
+++ randomForest.py
@@ -2,3 +2,97 @@
 Aqui vamos a poner 
 todo lo necesario para hacer fincionet RF a ppartir de competition 2
 '''
+import numpy as np
+import pandas as pd
+import sklearn as sk
+from sklearn import metrics
+from sklearn.model_selection import train_test_split, RandomizedSearchCV,GridSearchCV 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score,roc_curve, auc, roc_auc_score, f1_score
+from sklearn.calibration import calibration_curve, CalibrationDisplay
+
+def importDataSet(dataSetName):
+    '''
+    Import datasets and filling NaN values          
+    @input: DataSetName => The dataset path. 
+    @Output: Features(x) and tragets(y)    
+    ''' 
+    train = pd.read_csv(dataSetName, index_col = None)
+    y = train[['LABELS']]
+    y = y.fillna(0)
+    x = train.drop('LABELS', axis=1)
+    xMean = x.mean()
+    x = x.fillna(xMean)
+    return x, y
+
+def split(x,y,TestPercent):
+    x_train, x_validation, y_train, y_validation = train_test_split( x,y, test_size=TestPercent)
+    return x_train, x_validation, y_train, y_validation
+
+def printDataBalace(x_train, x_validation, y_train, y_validation):
+    ## Data shape exploration
+    print("",np.shape(x_train),"  :",np.shape(x_validation) )
+    print("Label balance on Training set: ", "\n", y_train['LABELS'].value_counts())
+    print("Label balance on Validation set: ", "\n", y_validation['LABELS'].value_counts())
+
+def predictOnFeaturesSet(model, featuresSet):
+    y_hat = model.predict(featuresSet)
+    return y_hat
+
+def investigateFeatureImportance(x_train, classifier, printed = True):
+    '''
+    @input: feature matrix
+            classifier trained
+            printed option: Default = True
+    @return: List of features ordered dessending by importance (pandas DF format). 
+    '''
+    features = x_train.columns
+    clasifierName = type(classifier).__name__
+    featuresInModel= pd.DataFrame({'feature': features,
+                   'importance': classifier.feature_importances_}).\
+                    sort_values('importance', ascending = False)
+    clasifierNameExtended = clasifierName + "_featureImportance.csv"     
+    featuresInModel.to_csv(clasifierNameExtended, index = None)
+    if printed:
+        with pd.option_context('display.max_rows', None,
+                       'display.max_columns', None,
+                       'display.precision', 3,
+                       ):
+            print(featuresInModel)
+    return featuresInModel
+
+
+
+## Trash ...
+def metric_RocAuc(y_probability, y_validation, estimator_name):
+    '''
+    Calculate and plt ROC metric
+    @argument: y_probability : the probability class=1.
+    @argument: y_validation: True labels.
+    fpr, tpr = false_positive, true_positive.
+    Return: "false_positive" and "true_positive", ROC_auc metric.
+    '''
+    fpr, tpr, _ = roc_curve(y_validation, y_probability) 
+    roc_auc = auc(fpr, tpr)
+    fig, axes = plt.subplots(constrained_layout=True,figsize=(5,3), dpi=150)
+    fig.suptitle(estimator_name)
+    axes.plot([0, 1], [0, 1], color= 'k',linestyle="--") # perfect fit 
+    display = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc,
+                                       estimator_name=estimator_name)
+    display.plot(ax=axes)
+    return fpr, tpr, roc_auc
+
+
+def evaluate_model(x_train, y_train, x_validation, y_validation, classifier):
+    features = x_train.columns
+    validation_Prediction = classifier.predict(x_validation)
+    validation_PredictedProb = classifier.predict_proba(x_validation)[:, 1]
+    ### ROC metric and curve #####
+    clasifierName = type(classifier).__name__
+    metric_RocAuc(validation_PredictedProb, y_validation,clasifierName)
+    fi_model = pd.DataFrame({'feature': features,
+                   'importance': classifier.feature_importances_}).\
+                    sort_values('importance', ascending = False)
+    clasifierNameExtended = clasifierName + "_info_fi.csv"     
+    fi_model.to_csv(clasifierNameExtended, index = None)
+    return fi_model
