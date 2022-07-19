0c5be49d313e1ab3f435cef8e3eb9e8ce58de076
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2022-07-18 23:07:25 -0400

    executeRFReg setted

--- executeRFReg.py
+++ executeRFReg.py
@@ -2,10 +2,13 @@ import pandas as pd
 import myServices 
 import randForReg as rfr
 
-def run():
+def main():
     datasetPath = './sample.csv'
-    rfReg  = rfr.implementRandomForestRegressor(datasetPath,'percentage', 0.2)
+    rfReg = rfr.implementRandomForestRegressor(datasetPath,'percentage', 0.2)
     x_train,x_validation,y_train, y_validation = rfReg.getSplitedDataset()
     rfr.printDataBalace(x_train, x_validation, y_train, y_validation,'percentage')
     bestEstimator = rfReg.fitRFRegressor()
     print(bestEstimator)
+
+if __name__ == "__main__":
+    main()
