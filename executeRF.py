e30562ee6c59559ac4ffbfff2844644ef2cf9690
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2022-07-22 10:32:01 -0400

    same

--- executeRF.py
+++ executeRF.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import myServices 
 import randForest as r
 import hydra
@@ -5,17 +6,26 @@ from omegaconf import DictConfig
 
 @hydra.main(config_path=f"config", config_name="config.yaml")
 def main(cfg: DictConfig):
+    log = pd.DataFrame()
     name = r.makeNameByTime()
     local = cfg.local
     pathDataset = local + cfg['pathDataset']
     percentOfValidation = cfg.percentOfValidation
+    # penalty = cfg.weightPenalty  ## UNCOMENT Onli for weighted fit
     arg = cfg.parameters
     frRegGS = r.implementRandomForestRegressor(pathDataset,'percentage', percentOfValidation, arg)
     best_estimator, r2 = frRegGS.fitRFRegressorGSearch()
-    print(r2)
-    _,x_validation,_,_ = frRegGS.getSplitedDataset()
-    r.investigateFeatureImportance(best_estimator,name,x_validation)
     r.saveModel(best_estimator,name)
+    _,x_validation,_,_ = frRegGS.getSplitedDataset()
+    casiffierName, featureImportance = r.investigateFeatureImportance(best_estimator,name,x_validation)
+    print(r2)
+
+    # Fill Log
+    log['dataset'] = cfg['pathDataset']
+    log['model+Id'] = name
+    log['classifierName'] = casiffierName, featureImportance
+    log['best_param'] = best_estimator
+    log['r2_score'] = r2  # UNCOMENT For Regression Only
 
 if __name__ == "__main__":
     with myServices.timeit():
