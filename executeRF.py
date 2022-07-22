f2855a45f388533effc2b248a06030903a958c19
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2022-07-22 14:25:19 -0400

    saving execution works

--- executeRF.py
+++ executeRF.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 import myServices 
 import randForest as r
 import hydra
@@ -12,20 +13,21 @@ def executeRFRegressor(cfg: DictConfig):
     percentOfValidation = cfg.percentOfValidation
     # penalty = cfg.weightPenalty  ## UNCOMENT Onli for weighted fit
     arg = cfg.parameters
-    frRegGS = r.implementRandomForestRegressor(pathDataset,'percentage', percentOfValidation, arg)
-    best_estimator, bestParameters, r2_validation= frRegGS.fitRFRegressorGSearch()
+    rForestReg = r.implementRandomForestRegressor(pathDataset,'percentage', percentOfValidation, arg)
+    best_estimator, bestParameters, r2_validation= rForestReg.fitRFRegressorGSearch()
     r.saveModel(best_estimator,name)
-    _,x_validation,_,_ = frRegGS.getSplitedDataset()
+    _,x_validation,_,_ = rForestReg.getSplitedDataset()
     regressorName, featureImportance = r.investigateFeatureImportance(best_estimator,name,x_validation)
     print(r2_validation)
     # Fill Log
     log['dataset'] = cfg['pathDataset']
     log['model_Id'] = name
     log['regressor_Name'] = regressorName,
-    log['best_param'] = bestParameters
-    log['features_Importance'] = featureImportance
+    bestP = json.dumps(bestParameters)
+    log['best_param'] = bestP
+    log['features_Importance'] = pd.DataFrame(featureImportance).to_string(justify= 'left')
     log['r2_score'] = r2_validation 
-    return best_estimator, log
+    return best_estimator,name,log
 
 def executeRFCalssifier(cfg: DictConfig):
     log = pd.DataFrame()
@@ -33,18 +35,18 @@ def executeRFCalssifier(cfg: DictConfig):
     local = cfg.local
     pathDataset = local + cfg['pathDataset']
     percentOfValidation = cfg.percentOfValidation
-    # penalty = cfg.weightPenalty  ## UNCOMENT Onli for weighted fit
     arg = cfg.parameters
-    frRegGS = r.implementRandomForestCalssifier(pathDataset,'percentage', percentOfValidation, arg)
-    _,x_validation,_,y_validation = frRegGS.getSplitedDataset()
-    best_estimator, bestParameters, r2_validation= frRegGS.fitRFClassifierGSearch()
+    rfClassifier = r.implementRandomForestCalssifier(pathDataset,'percentage', percentOfValidation, arg)
+    _,x_validation,_,y_validation = rfClassifier.getSplitedDataset()
+    best_estimator, bestParameters = rfClassifier.fitRFClassifierGSearch()
     classifierName, featureImportance = r.investigateFeatureImportance(best_estimator,name,x_validation)
-    accScore, macro_averaged_f1, micro_averaged_f1, ROC_AUC_multiClass = frRegGS.computeClassificationMetrics(best_estimator,x_validation,y_validation)
+    accScore, macro_averaged_f1, micro_averaged_f1, ROC_AUC_multiClass = rfClassifier.computeClassificationMetrics(best_estimator)
     log['dataset'] = cfg['pathDataset']
     log['model_Id'] = name
     log['regressor_Name'] = classifierName
-    log['best_param'] = bestParameters
-    log['features_Importance'] = featureImportance
+    bestP = json.dumps(bestParameters)
+    log['best_param'] = bestP
+    log['features_Importance'] = featureImportance.ravel()
     log['Accuraci_score'] = accScore
     log['macro_averaged_f1'] = macro_averaged_f1
     log['micro_averaged_f1'] = micro_averaged_f1
@@ -52,9 +54,9 @@ def executeRFCalssifier(cfg: DictConfig):
 
 @hydra.main(config_path=f"config", config_name="config.yaml")
 def main(cfg: DictConfig):
-    best_estimator, log = executeRFRegressor(cfg)
-    r.saveModel(best_estimator)
-    log.to_csv({best_estimator.__name__}+'.csv')
+    best_estimator,name, log = executeRFRegressor(cfg)
+    r.saveModel(best_estimator, name)
+    log.to_csv(name +'.csv') 
 
 if __name__ == "__main__":
     with myServices.timeit():
