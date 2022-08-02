745bf3831a10c4745c36f7da1921b0ba0abaf112
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2022-08-02 10:47:23 -0400

    FAcc Normalized

--- samplingControlTools.py
+++ samplingControlTools.py
@@ -1,7 +1,18 @@
+import pandas as pd
+from numpy import where
+from imblearn.under_sampling import NearMiss
 
 class underSampling():
-    def __init__(self) -> None:
+    def __init__(self, dataset, targetCol) -> None:
+        self.dataset = dataset
+        self.targetCol = targetCol
         return
+    
+    def stratifiedUndersampling(self, majotityClass):
+        row_ix = where(self.dataset[self.targetCol] == majotityClass)[0]
+        df = self.datset[row_ix,:]  # Extract features of majority class
+        ### in progress   ####
+        return df 
 
 
 class overSampling():
