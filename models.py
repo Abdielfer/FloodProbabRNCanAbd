29aaf64b22a9a93912bf53822f70eddd39bc07e3
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2022-10-12 15:40:33 -0400

    more fine tunning

--- models.py
+++ models.py
@@ -172,10 +172,10 @@ class implementingMLPCalssifier():
         power_t=0.5, max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True, 
         early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, n_iter_no_change=10, max_fun=15000)[source]
         '''
-        args = [{'random_state':'50', 'hidden_layer_sizes':(16,32,64,32,16),
+        params = {'random_state':50, 'hidden_layer_sizes':(200,150,100,50),
                 'early_stopping':True,'verbose':True,
-                'tol':0.00010,'validation_fraction':0.2,' warm_start':True}]
-        mlpClassifier = MLPClassifier(**args)
+                'tol':0.00010,'validation_fraction':0.2,'warm_start':True}
+        mlpClassifier = MLPClassifier(**params)
         return mlpClassifier
 
     def fitMLPClassifier(self):
