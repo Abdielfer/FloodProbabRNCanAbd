ed930b09b652c33b130cc63c71f4fb1d225d2f36
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2023-01-31 15:44:25 -0500

    Compute area for multipolygons DONEE!!!!!

--- vectorManipulation.py
+++ vectorManipulation.py
@@ -1,5 +1,7 @@
 import geopandas as gpd
 from shapely.geometry import shape, Polygon
+import utm
+from pyproj import CRS
 
 class importSHPFIles():
     def __init__(self, pathSHP:str) -> None:
@@ -27,11 +29,29 @@ def readSHP(pathSHP:str):
     shpFile = gpd.read_file(pathSHP)
     return shpFile
 
-def computePolygonArea(polygon):
+
+def findtheutm(aGeometry):
+    '''
+    A function to find a coordinates UTM zone
+    '''
+    
+    x, y, parallell, latband = utm.from_latlon(aGeometry.centroid.y, aGeometry.centroid.x)
+    if latband in 'CDEFGHJKLM': #https://www.lantmateriet.se/contentassets/379fe00e09d74fa68550f4154350b047/utm-zoner.gif
+        ns = 'S'
+    else:
+        ns = 'N'
+    crs = "+proj=utm +zone={0} +{1}".format(parallell, ns) #https://gis.stackexchange.com/questions/365584/convert-utm-zone-into-epsg-code
+    crs = CRS.from_string(crs)
+    _, code = crs.to_authority()
+    return int(code)
+    
+def computeGeometrArea(gpdFrame: gpd.geodataframe.GeoDataFrame):
     '''
         You must extrac de polygon from the GeoDataFrame befor passing to the function.
     '''
-    return shape(polygon).area
+    epsg = findtheutm(gpdFrame.geometry.iloc[0])
+    gpdFrame['Area_m2'] = gpdFrame.to_crs(epsg).area
+    return gpdFrame['Area_m2']
 
 def addAreaColum(gpdFrame: gpd.geodataframe.GeoDataFrame, factor = 1) -> None:
     '''
@@ -40,7 +60,4 @@ def addAreaColum(gpdFrame: gpd.geodataframe.GeoDataFrame, factor = 1) -> None:
     '''
     for i in range(len(gpdFrame)):
 
-    
-
-    
         pass 
