4872f79c45809a9c1dd9aa9c3fdf9e560448599d
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2023-01-31 10:37:15 -0500

    computePolygonArea

--- vectorManipulation.py
+++ vectorManipulation.py
@@ -1,14 +1,21 @@
 import geopandas as gpd
+from shapely.geometry import shape
 
 
 class importSHPFIles():
     def __init__(self, pathSHP:str) -> None:
         self.SHPObject = readSHP(pathSHP)
-        self.headAttributeTable = self.SHPObject.head(5)
+        
         pass
+    
+    def print_headAttributeTable(self):
+        print(self.SHPObject.head(5))
 
+    def getCRS(self):
+        return self.SHPObject.crs
 
-
+    def getGeometryType(self):
+        return self.SHPObject.geom_type
 
 
 #### General use functions    ####
@@ -16,3 +23,5 @@ def readSHP(pathSHP:str):
     shpFile = gpd.read_file(pathSHP)
     return shpFile
 
+def computePolygonArea(polygon):
+    return shape(polygon).area
